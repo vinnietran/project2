0ad49e78e6d9a9c46be0b9c7546814e9e7c31617
VinTran <tranquillo.vinnie@gmail.com>
2019-05-01 19:32:03 -0400

    added a table to the schema. will add more

--- models/schema.sql
+++ models/schema.sql
@@ -4,4 +4,10 @@ CREATE DATABASE exampledb;
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
 
-
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT NOT NULL, 
+    fname VARCHAR (50) NOT NULL, 
+    lname VARCHAR (50) NOT NULL, 
+    createdAt TIMESTAMP NOT NULL, 
+    primary key(user_id)
+)
