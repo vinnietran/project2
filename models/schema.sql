d386573024587cad851afba9c3ad022c6c713322
Jin Kim <jkim1665@gmail.com>
2019-05-07 20:00:28 -0400

    employerfolder

--- models/schema.sql
+++ models/schema.sql
@@ -1,11 +1,3 @@
 DROP DATABASE IF EXISTS exampledb;
 CREATE DATABASE exampledb;
-use exampledb;
 
-CREATE TABLE users (
- id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR (100) NOT NULL, 
-    email VARCHAR (100) NOT NULL, 
-    password VARCHAR (100) NOT NULL, 
-  PRIMARY KEY (id)
-);
