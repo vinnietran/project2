6aee74f01d134dd47fa8e4a8e62fc435e3f63ccc
VinTran <tranquillo.vinnie@gmail.com>
2019-05-10 00:46:22 -0400

    Added new employee page

---
+++ public/js/init.js
@@ -0,0 +1,111 @@
+//given
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+//jobs modal
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, options);
+});// Or with jQuery
+
+
+//*******date
+$(document).ready(function(){
+  $('.modal').modal();
+});
+
+// $(document).ready(function () {
+//   //MAKE SURE YOU CALL .leanModal METHOD THIS WAY.
+//   //ELSE MODAL MAP WILL NOT SHOW PROPERLY.
+//     $('.modal-trigger').leanModal({
+//         ready: function () {
+//             var map2 = document.getElementById("map");
+//             google.maps.event.trigger(map2, 'resize');
+//         }
+//     });
+// });
+     
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.datepicker');
+  var instances = M.Datepicker.init(elems, options);
+});// Or with jQuery
+
+$(document).ready(function(){
+  $('.datepicker').datepicker();
+});
+        
+//*******time
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.timepicker');
+  var instances = M.Timepicker.init(elems, options);
+});// Or with jQuery
+
+$(document).ready(function(){
+  $('.timepicker').timepicker();
+});
+
+//******carousel 
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
+    
+$(document).ready(function () {
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function (event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+
+          // Store hash
+          var hash = this.hash;
+
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+              scrollTop: $(hash).offset().top
+          }, 900, function () {
+
+              // Add hash (#) to URL when done scrolling (default click behavior)
+              window.location.hash = hash;
+          });
+      } // End if
+  });
+
+  $(window).scroll(function () {
+      $(".slideanim").each(function () {
+          var pos = $(this).offset().top;
+
+          var winTop = $(window).scrollTop();
+          if (pos < winTop + 600) {
+              $(this).addClass("slide");
+          }
+      });
+  });
+
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('select').formSelect();
+});
